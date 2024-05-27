dcf0976fe414027baf25e32ac780f4362878309e
Frank Prechtel <frank@prechtel.eu>
2024-05-27 10:22:25 +0200

    fix Keycloak container image startup command

--- setup/run_keycloak_docker.sh
+++ setup/run_keycloak_docker.sh
@@ -5,4 +5,4 @@ export WORKSHOP_HOME=/home/afa/Development/localgit/secure-oauth2-oidc-workshop/
 
 docker run --rm -p 8080:8080 -e DB_VENDOR=h2 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin \
     -e KEYCLOAK_IMPORT=/tmp/keycloak_realm_workshop.json \
-    -v $WORKSHOP_HOME/keycloak_realm_workshop.json:/tmp/keycloak_realm_workshop.json quay.io/keycloak/keycloak:24.0.4
+    -v $WORKSHOP_HOME/keycloak_realm_workshop.json:/tmp/keycloak_realm_workshop.json quay.io/keycloak/keycloak:24.0.4 start-dev

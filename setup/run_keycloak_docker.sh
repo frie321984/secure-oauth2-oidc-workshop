2a2ddbe5bd01fb99865976ebfc00752f6a9f4f21
Frank Prechtel <frank@prechtel.eu>
2024-05-27 10:27:18 +0200

    fix Keycloak container parameter

--- setup/run_keycloak_docker.sh
+++ setup/run_keycloak_docker.sh
@@ -3,6 +3,6 @@
 # replace this config with your own directory
 export WORKSHOP_HOME=/home/afa/Development/localgit/secure-oauth2-oidc-workshop/setup
 
-docker run --rm -p 8080:8080 -e DB_VENDOR=h2 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin \
+docker run --rm -p 8080:8080 -e DB_VENDOR=h2 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin \
     -e KEYCLOAK_IMPORT=/tmp/keycloak_realm_workshop.json \
     -v $WORKSHOP_HOME/keycloak_realm_workshop.json:/tmp/keycloak_realm_workshop.json quay.io/keycloak/keycloak:24.0.4 start-dev
